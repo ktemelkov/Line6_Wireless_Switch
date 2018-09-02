dabd3a60ff900ebb17f309adb127618fca739b60
Kalin Temelkov <kteme@DESKTOP-VCPSS1G.hilscherdtc.local>
2018-09-02 17:09:00 +0300

    folder structure with PlatformIO

---
+++ src/guitar/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "../include/const.h"
+
+
+
+/**
+ * 
+ */
+void setup() {
+    // put your setup code here, to run once:
+}
+
+
+
+/**
+ * 
+ */
+void loop() {
+    // put your main code here, to run repeatedly:
+}
--- src/pedal/main.cpp
+++ src/pedal/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include "../include/const.h"
 
+
+/**
+ * 
+ */
 void setup() {
     // put your setup code here, to run once:
 }
 
+
+
+/**
+ * 
+ */
 void loop() {
     // put your main code here, to run repeatedly:
 }
